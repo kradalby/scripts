90e49918c4969174a03f4c95bcccddc19cabf779
Kristoffer Dalby <kradalby@kradalby.no>
2014-05-01 17:26:59 +0200

    initial bs.sh

---
+++ bs.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# This script is written for personal use,
+# it is mainly for bootstrapping a server
+# with the utilites that i use and want
+# on a newly installed Debian linux server.
+
+# Usage:
+# curl http://kradalby.no/bs.sh | sh
+
+SUDO=''
+
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+    if [ -x /usr/bin/sudo ]; then
+        SUDO='sudo'
+    else
+        exit "You are not root, and sudo is not installed"
+    fi
+fi
+
+# Written by a awesome person from dotKom
+function progress() {
+    if $VERBOSE
+    then
+        $@
+    else
+        cmd="$@"
+        $@ &> /dev/null &
+        pid=$!
+        spinner='-\|/'
+
+        i=0
+        while kill -0 $pid &> /dev/null
+        do
+            i=$(( (i+1) %4 ))
+            printf "\r\t%-${LJUST_COLS}.${LJUST_COLS}s %${RJUST_COLS}s" "${cmd}" "[ ${spinner:$i:1}${spinner:$i:1} ]"
+            sleep .1
+        done
+
+        if [ $? -ne 0 ]
+        then
+            echo "return code $?";
+        fi
+        printf "\r\t%-${LJUST_COLS}.${LJUST_COLS}s %${RJUST_COLS}s\n" "${cmd}" "[ OK ]"
+    fi
+}
+
+function update() {
+    echo "Updating repositories and OS"
+    progress $SUDO apt-get update
+    progress $SUDO apt-get dist-upgrade -y 
+}
+
+function install_packages() {
+    echo "Installing packages from apt"
+    progress $SUDO apt-get install -y \
+        tmux vim-nox python python3 git \
+        htop zsh python-dev python-setuptools \
+        ntp tree ncdu ack
+}
+
+function install_ohzsh() {
+    echo "Installing Oh My ZSH for this user"
+    progress curl -L http://install.ohmyz.sh | sh
+    
+    if [[ $EUID -ne 0]]; then
+        echo "Installing Of My ZSH for root"
+        sudo su -c curl -L http://install.ohmyz.sh | sh
+    fi
+}
+
+
+
