699b09277d4f1274d4039427aaa2f40f4e7d7813
Kristoffer Dalby <kradalby@kradalby.no>
2014-05-12 15:17:36 +0200

    initial fix

---
+++ fixsound.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is for reloading the sounddrivers on my macbook pro
+
+sudo kextunload /System/Library/Extensions/AppleHDA.kext
+sudo kextload /System/Library/Extensions/AppleHDA.kext
+
