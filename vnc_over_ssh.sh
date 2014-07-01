1b50d803074d851255a2e8b1118f2dae16c4bf54
Kristoffer Dalby <kradalby@kradalby.no>
2014-07-01 22:43:45 +0200

    a basic script to open vnc over ssh

---
+++ vnc_over_ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Force ssh into background
+eval "ssh kradalby@192.168.1.148 -L 6000:localhost:6000 -N &"
+
+# Get pid
+sshpid=$!
+
+# Open vnc and wait for it to close
+open -W vnc://localhost:6000
+
+# kill ssh
+kill $sshpid
