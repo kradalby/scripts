8919b9c88b60f67bbb79baf0d9c9bc1cd9cf9aa4
Kristoffer Dalby <kradalby@kradalby.no>
2014-12-06 08:47:51 +0100

    bifurcation

--- ddns.sh
+++ ddns.sh
@@ -7,12 +7,12 @@ record_id="3521543"
 #your api key
 api_key="6fe23b7ae111a962f16f37a3c023ef8434074ff228a6b08fbdc1d952f8884e86"
 
-domain_id=$1
-record_id=$2
-api_key=$3
+#domain_id=$1
+#record_id=$2
+#api_key=$3
 
 ### don't change ###
 ip="$(curl --silent http://canihazip.com/s)"
 curl --silent -H "Authorization: Bearer $api_key" -H "Content-Type: application/json" \
     -d '{"data": "'"$ip"'"}' \
-    -X PUT "https://api.digitalocean.com/v2/domains/$domain_id/records/$record_id"
+    -X PUT "https://api.digitalocean.com/v2/domains/$domain_id/records/$record_id" > /dev/null
