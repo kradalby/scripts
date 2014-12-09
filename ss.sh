c397a37830fd0c9d3dc3676d2a7c96a9670f014b
Kristoffer Dalby <kradalby@kradalby.no>
2014-12-09 19:57:31 +0100

    This is the last time we let Stephen commit ascii porn in the comments.

--- ss.sh
+++ ss.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Use this as a osx service.
+# Edit for own needs.
+# 
+# Install:
+# Open Automater.app
+# Create Service
+# Change to no input and add Run Shell Script
+# Paste path to script ( make sure it is executable )
+# Save and add a shortcut to it under System Preferences > Keyboard > Shortcuts
+
 url=https://kradalby.no/ss/
 filename=ss_`date '+%Y%m%d%H%M%S'`.png
 path=~/Pictures/ss/
