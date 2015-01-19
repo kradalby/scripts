2e0e71118666022a42b6ac21d68a0079f9acf044
Kristoffer Dalby <kradalby@kradalby.no>
2015-01-19 11:00:49 +0100

    fix /sigh

--- ss.sh
+++ ss.sh
@@ -15,5 +15,5 @@ filename=`date '+%Y%m%d%H%M%S'`.png
 path=~/Pictures/ss/
 mkdir -p $path
 screencapture -o -i $path$filename
-scp $path$filename onyx:/srv/www/kradalby.no/ss/ > /dev/null
+scp $path$filename onyx:/srv/www/kradalby.no/ss/
 printf $url$filename | pbcopy
