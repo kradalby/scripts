b89757fa994e68476e4c3f70f7ada7ccfa299ac4
Kristoffer Dalby <kradalby@kradalby.no>
2015-01-23 18:45:13 +0100

    bump to 0.0.3-dev:wq

--- pb.sh
+++ pb.sh
@@ -10,12 +10,7 @@
 # Paste path to script ( make sure it is executable )
 # Save and add a shortcut to it under System Preferences > Keyboard > Shortcuts
 
-url=https://kradalby.no/ss/
-filename=`date '+%Y%m%d%H%M%S'`.png
-path=~/Pictures/ss/
-mkdir -p $path
-screencapture -o -i $path$filename
-eval "scp -v $path$filename onyx:/srv/www/kradalby.no/ss/"
-derp=!$
-echo=$derp
+url=https://kradalby.no/pb/
+filename=`date '+%Y%m%d%H%M%S'`.txt
+echo $1 | ssh onyx "cat > /srv/www/kradalby.no/pb/$filename"
 printf $url$filename | pbcopy
