f695d836d2fb604b94d924a3c3b306adfa76d971
Kristoffer Dalby <kradalby@kradalby.no>
2015-08-06 16:36:26 +0200

    added scriptto start apps after imac crash

---
+++ start_apps_after_crash_stopid_imac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+APPS="Safari iTerm Mail Messages Messenger Skype Slack Spotify Transmission"
+
+# Stopid google with space name
+open /Applications/Google\ Chrome.app
+
+for app in $APPS
+do
+    open /Applications/$app.app
+done
