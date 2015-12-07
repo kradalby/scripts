87419ad133da1d34b76808f18363abdfac0307d3
Kristoffer Dalby <kradalby@kradalby.no>
2015-12-07 08:03:14 +0100

    updated screenshot script to onyx :stuck_out_tongue_closed_eyes:

--- ss.sh
+++ ss.sh
@@ -15,5 +15,5 @@ filename=`date '+%Y%m%d%H%M%S'`.png
 path=~/Pictures/ss/
 mkdir -p $path
 screencapture -o -i $path$filename
-scp -v $path$filename gengar:/srv/www/kradalby.no/ss/
+scp -v $path$filename onyx:/usr/local/www/kradalby.no/ss/
 printf $url$filename | pbcopy
